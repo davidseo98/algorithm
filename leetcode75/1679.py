bf8db9a00aa1f9380971678d6080529584b04656
davidseo98 <hyundukseo98@gmail.com>
2025-01-16 06:27:03 +0900

    [LeetCode75] 1679 better solution

--- leetcode75/1679.py
+++ leetcode75/1679.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 class Solution(object):
     def maxOperations(self, nums, k):
         """
@@ -26,4 +28,28 @@ class Solution(object):
                 s += 1
         
         return cnt
+    
+    # Beats 82.26% (runtime)
+    def maxOperations(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+
+        cnt_dict = defaultdict(int)
+        answer = 0
+
+        for num in nums:
+            cnt_dict[num] += 1
+        
+        for key in cnt_dict.keys():
+            cur, remain = cnt_dict[key], cnt_dict[k-key]
+            if cur and remain:
+                increment = min(cur, remain) if key != k-key else cur//2
+                cnt_dict[key] -= increment
+                cnt_dict[k-key] -= increment
+                answer += increment
+        
+        return answer
 
